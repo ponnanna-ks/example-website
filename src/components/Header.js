60ddd402901042f9796c21d2fa9a3f76c1b79d96
Deekshithara <162873432+Deekshithara@users.noreply.github.com>
2024-06-22 16:36:19 +0530

    header changes
    ok bye

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-scroll';
-//import './Header.css'; // Assuming you have a CSS file for styling
+import './styles/header.css';
 
 const Header = ({ scrollToSection }) => (
   <header className="header">
     <nav>
-      <ul>
+      <ul className='header_link'>
         <li><Link to="home" smooth={true} duration={800}>Home</Link></li>
         <li><Link to="cottages" smooth={true} duration={800}>Cottages</Link></li>
         <li><Link to="activities" smooth={true} duration={800}>Activities</Link></li>
